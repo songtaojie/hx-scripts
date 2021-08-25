91bc7d5d1396b567445893f278b2f342ac9ca8b9
songtaojie <stjworkemail@163.com>
2021-08-25 16:50:14 +0800

    添加jenkins镜像构建脚本

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ declare -A sysdict=(
     ["0"]="build"
     ["1"]="publish"
 		["2"]="nginx"
+		["3"]="jenkins"
   )
 
 echo -e "\033[33m all support system: \033[0m"
@@ -29,6 +30,11 @@ case $MYSYS in
 	;;
 2) #Nginx
 	cd nginx
+	docker-compose up -d --build 
+	break
+	;;
+3) #jenkins
+	cd jenkins
 	docker-compose up -d --build 
 	break
 	;;
