0ef966202e8514c1d010a0ae58a7896963a91816
songtaojie <stjworkemail@163.com>
2021-11-19 14:54:31 +0800

    update

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,7 +4,6 @@ cd ..
 git pull
 cd $curpwd
 
-source ./.env
-docker-compose up --build -d --force-recreate
-# docker-compose -f docker-compose.yml up --build -d --force-recreate
+docker-compose --env-file ./.env up --build -d --force-recreate
+
 docker system prune -f
